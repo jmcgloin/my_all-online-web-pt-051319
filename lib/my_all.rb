db4cdd8d022ae365cf336a4e6b9cd89aea556ee2
Jason McGloin <jason.mcgloin@gmail.com>
2019-05-14 20:20:09 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,5 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1
   end
-  return !!block_return_values.include?(false)
+  return !block_return_values.include?(false)
 end
